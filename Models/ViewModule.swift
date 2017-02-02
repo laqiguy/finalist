ec8d2a5d5e8aebece90e6793b3593a368d12566e
Aleksey Tyurnin <turlex251@gmail.com>
2017-02-02 14:59:49 +0500

    module specification updated ViewModule protocol updated

--- Models/ViewModule.swift
+++ Models/ViewModule.swift
@@ -1,9 +1,11 @@
-protocol {ViewModuleName}: class{ //ROOT_CLASS
-	var vc: UIViewController {get} //ROOT_VC
-	
+protocol {ViewModuleName}: ViewModule{ //ROOT_CLASS	
 	func initialize() //ROOT_INITIALIZE
 
 	weak var {http_module_name}: HTTP! //MODULE_ HTTP
 	weak var {sip_module_name}: SIP! //MODULE_HTTP
 	weak var {rtsp_module_name}: RTSP? //MODULE_RTSP
+}
+
+protocol ViewModule: class{
+	var vc: ViewController {get}
 }
